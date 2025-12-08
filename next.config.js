250282190b5c511297564b1d73645d5af6de0347
Burlarad <burlarad@vcu.edu>
2025-12-07 21:40:49 -0500

    Fix build: externalize swisseph native addon in Next config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   images: {
     domains: [],
   },
+
+  webpack: (config, { isServer }) => {
+    // Externalize swisseph native addon on server builds so webpack doesn't try to bundle the .node binary
+    if (isServer) {
+      config.externals = config.externals || [];
+      config.externals.push("swisseph");
+    }
+    return config;
+  },
 }
 
 module.exports = nextConfig
