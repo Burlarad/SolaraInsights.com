f5fe5220b20add9bbe4b9201391568b441392de3
Burlarad <burlarad@vcu.edu>
2026-01-06 18:41:41 -0500

    chore: cleanup dead code, prune unused deps, fix local auth redirects

--- components/charts/index.ts
+++
@@ -1,6 +0,0 @@
-// Chart visualization components for astrology
-export { BalanceCharts } from "./BalanceCharts";
-export { AspectGrid, AspectList } from "./AspectGrid";
-export { NatalWheel, ZodiacRing } from "./NatalWheel";
-export type { AspectPlacement, AspectType } from "./AspectGrid";
-export type { PlanetPlacement, HousePlacement, Angles, AspectData } from "./NatalWheel";
